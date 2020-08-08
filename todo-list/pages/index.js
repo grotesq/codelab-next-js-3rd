f7c7966798a088f4cebc577e5df4e86743bd1bbb
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2020-08-08 11:50:37 +0900

    수정 : 완료 체크박스

--- todo-list/pages/index.js
+++ todo-list/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Head from 'next/head';
 import _ from 'lodash';
+import produce from 'immer';
 
 export default function Home() {
+  // hook 은 if, for 같은 제어문 내에서 사용할 수 없음
   const [list, setList] = React.useState([]);
   const [text, setText] = React.useState('');
   // console.log( window ); // 서버사이드 (X) 클라이언트 사이드 (O)
   React.useEffect(()=>{
+    // 컴포넌트가 화면에 표시될 때
     let result = localStorage.getItem( 'todo-list' );
     if( !result ) {
       result = [];
@@ -23,8 +26,18 @@ export default function Home() {
   }, []);
 
   React.useEffect(() => {
+    // list 라는 주시 대상이 변경이 있었을 때
     localStorage.setItem( 'todo-list', JSON.stringify( list ) );
   }, [list]);
+
+  // add 함수가 렌더가 일어날 때 마다 생성
+  /*
+  console.log( '함수 생성' );
+  const add = () => {
+    // ...
+    setText( '' );
+  }
+  */
 
   const addItem = React.useCallback( () => {
     const item = {
@@ -41,6 +54,14 @@ export default function Home() {
   const removeItem = React.useCallback( id => {
     setList( _.reject( list, item => item.id === id ) );
   }, [list]);
+
+  const done = React.useCallback( id => {
+    setList( produce( list, draft => {
+      const target = list.find( item => item.id === id );
+      const index = list.indexOf( target );
+      draft[ index ].isDone = !target.isDone;
+    } ) );
+  }, [list] );
   
   return (
     <div className="py-8 px-16">
@@ -68,6 +89,12 @@ export default function Home() {
         {
           list.map( item => (
             <li key={ item.id }>
+              <input
+                type="checkbox"
+                className="mr-2"
+                checked={ !!item.isDone }
+                onChange={ () => done( item.id ) }
+              />
               { item.text }
               <button className="ml-2 text-xs text-red-500"
                 onClick={ () => removeItem( item.id ) }
@@ -81,3 +108,19 @@ export default function Home() {
     </div>
   )
 }
+
+/*
+import produce from 'immer';
+
+const obj = {
+  string: 'value',
+  number: 123,
+};
+
+const newObj = produce( obj, draft => {
+  draft.number = 100;
+} );
+
+console.log( obj ); // { string: 'value', number: 123 }
+console.log( newObj ); // { string: 'value', number: 100 }
+*/
